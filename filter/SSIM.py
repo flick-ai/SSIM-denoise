df93a2428d02ce8e8558044b8e3d071134c2ed34
Flick <flick-lu@sjtu.edu.cn>
2022-03-20 15:06:04 +0800

    Merge branch 'master' of https://github.com/flick-ai/SSIM-denoise

--- filter/SSIM.py
+++ filter/SSIM.py
@@ -72,7 +72,7 @@ def SSIM_HSV(src, sample, i, f=10, t=2):
             weight_s = cal(image[:, :, 1], w_image[:, :, 1], eng)
             weight_v = cal(image[:, :, 2], w_image[:, :, 2], eng)
             weight = (weight_s ** i + weight_v ** i) ** (1 / i)
-            sum_image = sum_image + np.array([weight[f:f + H, f:f + W] ** i]).reshape((512, 512, 1)) * w_image[f:f + H, :f + W, :]
+            sum_image = sum_image + np.array([weight[f:f + H, f:f + W] ** i]).reshape((512, 512, 1)) * w_image[f:f + H, f:f + W, :]
             sum_weight = sum_weight + np.array([weight[f:f + H, f:f + W] ** i]).reshape((512, 512, 1))
     out = sum_image / sum_weight
     return out
